d7e4732b980885bfc823740c76cab14baf369a12
Benjamin <ben639dx@gmail.com>
2017-10-04 19:30:13 +0200

    goog

---
+++ googltest.py
@@ -0,0 +1,21 @@
+lst = [7,6,4]
+find = []
+i = 0;
+s = 8
+
+
+def findme(i, s, lst):    
+    while (i < len(lst)):
+        if (s - lst[i] in find) :
+            return True;
+        else :
+            find.append(lst[i])
+        i = i + 1
+    return False;
+
+
+if (findme(i, s, lst)):
+    print ("OK")
+else :
+    print("KO")
+
